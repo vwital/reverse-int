02401127535abf57d018cf06fdab39c043bc4591
vwital <wital128@tut.by>
2022-07-01 20:12:21 +0300

    init: start reverse-init task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    n = Math.abs(n);
+    let string = n.toString();
+    let result = "";
+    let len = string.length;
+    for (i = 0; i < len; i++) {
+        result = string[i] + result;
+    }
+    return result * 1;
+};
